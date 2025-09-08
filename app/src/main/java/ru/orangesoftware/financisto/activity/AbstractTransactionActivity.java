6c750b8435431892fc394b24325bc55b4e929f80
Moin Ul Islam Asmi <moin.asmi2020@gmail.com>
2025-09-09 00:10:32 +0530

    Versions update - build fixes

--- app/src/main/java/ru/orangesoftware/financisto/activity/AbstractTransactionActivity.java
+++ app/src/main/java/ru/orangesoftware/financisto/activity/AbstractTransactionActivity.java
@@ -193,7 +193,7 @@ public abstract class AbstractTransactionActivity extends AbstractActivity imple
                     dateTime.get(Calendar.DAY_OF_MONTH)
             );
             applyTheme(this, dialog);
-            dialog.show(getFragmentManager(), "DatePickerDialog");
+            dialog.show(getSupportFragmentManager(), "DatePickerDialog");
         });
 
         timeText = findViewById(R.id.time);
@@ -209,7 +209,7 @@ public abstract class AbstractTransactionActivity extends AbstractActivity imple
                     dateTime.get(Calendar.HOUR_OF_DAY), dateTime.get(Calendar.MINUTE), is24Format
             );
             applyTheme(this, dialog);
-            dialog.show(getFragmentManager(), "TimePickerDialog");
+            dialog.show(getSupportFragmentManager(), "TimePickerDialog");
         });
 
         internalOnCreate();
